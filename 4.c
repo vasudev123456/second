851c796b9d57b9e21bf6a2664e2ad4cefcc8afdf
vasudev123456 <vasudevramoji7@gmail.com>
2025-11-26 18:11:40 +0530

    4.c

--- 4.c
+++ 4.c
@@ -1,44 +1,62 @@
-#include <stdio.h>
-#include <stdbool.h>
+import java.util.Scanner;  
+import java.io.*; 
+public class crc1 
+{ 
+public static void main(String args[])  
+{  
+Scanner sc = new Scanner(System.in);  
+ 
+System.out.print("Enter message bits: ");  
+String message = sc.nextLine();  
+System.out.print("Enter generator: "); 
+String generator = sc.nextLine();  
+int data[] = new int[message.length() + generator.length() - 1]; 
+int divisor[] = new int[generator.length()];  
+for(int i=0;i<message.length();i++)  
+data[i] = Integer.parseInt(message.charAt(i)+"");  
+for(int i=0;i<generator.length();i++) 
+divisor[i] = Integer.parseInt(generator.charAt(i)+""); 
 
-#define V 6
-#define INF 99999
+for(int i=0;i<message.length();i++) 
+{ 
+if(data[i]==1) for(int j=0;j<divisor.length;j++) 
+data[i+j] ^= divisor[j];  
+} 
 
-int minDist(int dist[], bool spt[]) {
-    int min = INF, idx = -1;
-    for (int i = 0; i < V; i++)
-        if (!spt[i] && dist[i] <= min)
-            min = dist[i], idx = i;
-    return idx;
-}
-
-void dijkstra(int g[V][V], int src) {
-    int dist[V]; bool spt[V] = {0};
-    for (int i = 0; i < V; i++) dist[i] = INF;
-    dist[src] = 0;
-
-    for (int i = 0; i < V - 1; i++) {
-        int u = minDist(dist, spt);
-        spt[u] = true;
-        for (int v = 0; v < V; v++)
-            if (!spt[v] && g[u][v] && dist[u] + g[u][v] < dist[v])
-                dist[v] = dist[u] + g[u][v];
-    }
-
-    printf("Vertex Distance from %d\n", src);
-    for (int i = 0; i < V; i++)
-        printf("%d \t %d\n", i, dist[i]);
-}
-
-int main() {
-    int g[V][V] = {
-        {0, 4, 0, 0, 0, 0},
-        {4, 0, 8, 0, 0, 0},
-        {0, 8, 0, 7, 0, 4},
-        {0, 0, 7, 0, 9, 14},
-        {0, 0, 0, 9, 0, 10},
-        {0, 0, 4, 14, 10, 0}
-    };
-    dijkstra(g, 0);
-    return 0;
+System.out.print("The checksum code is: ");  
+for(int i=0;i<message.length();i++)  
+data[i] = Integer.parseInt(message.charAt(i)+"");  
+for(int i=0;i<data.length;i++)  
+System.out.print(data[i]);  
+System.out.println(); 
+ 
+System.out.print("Enter checksum code: ");  
+message = sc.nextLine();  
+System.out.print("Enter generator: ");  
+generator = sc.nextLine();  
+data = new int[message.length() + generator.length() - 1];  
+divisor = new int[generator.length()];  
+for(int i=0;i<message.length();i++)  
+data[i] = Integer.parseInt(message.charAt(i)+"");  
+for(int i=0;i<generator.length();i++)  
+divisor[i] = Integer.parseInt(generator.charAt(i)+""); 
+r 
+for(int i=0;i<message.length();i++)  
+{  
+if(data[i]==1) for(int j=0;j<divisor.length;j++)  
+data[i+j] ^= divisor[j]; 
+} 
+ 
+boolean valid = true; 
+for(int i=0;i<data.length;i++)  
+if(data[i]==1) 
+{ 
+valid = false;  
+break;  
+} 
+if(valid==true)  
+System.out.println("Data stream is valid");  
+else 
+System.out.println("Data stream is invalid. CRC error occurred.");  
+} 
 }
