ea2a5f23ed396707749a2f2cfad92370d3dd7c05
vasudev123456 <vasudevramoji7@gmail.com>
2025-11-26 18:09:18 +0530

    9.c

--- 11.c
+++ 11.c
@@ -1,99 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+import java.util.*; 
+import java.io.*; 
+public class rsa 
+{ 
+static int gcd(int m,int n) 
+{ 
+while(n!=0) 
+{ 
+int r=m%n; 
+m=n; 
+n=r; 
+} 
+return m; 
+} 
+ 
+public static void main(String args[]) 
+{ 
+int p=0,q=0,n=0,e=0,d=0,phi=0; 
+int nummes[]=new int[100]; 
+int encrypted[]=new int[100]; 
+int decrypted[]=new int[100]; 
+int i=0,j=0,nofelem=0; 
+Scanner sc=new Scanner(System.in); 
+String message ; 
+System.out.println("Enter the Message to be encrypted:"); 
+message= sc.nextLine(); 
+System.out.println("Enter value of p and q\n"); 
+p=sc.nextInt(); 
+q=sc.nextInt(); 
+n=p*q; 
+phi=(p-1)*(q-1); 
+for(i=2;i<phi;i++) 
+if(gcd(i,phi)==1) break; 
+e=i; 
+for(i=2;i<phi;i++) 
+if((e*i-1)%phi==0) 
+break; 
+d=i; 
+for(i=0;i<message.length();i++) 
+{ 
+char c = message.charAt(i); 
+int a =(int)c; 
+nummes[i]=c-96; 
+} 
+nofelem=message.length(); 
+for(i=0;i<nofelem;i++) 
+{ 
+encrypted[i]=1; 
+for(j=0;j<e;j++) 
+encrypted[i] =(encrypted[i]*nummes[i])%n; 
+} 
+System.out.println("\n Encrypted message\n"); 
+for(i=0;i<nofelem;i++) 
+{ 
+System.out.print(encrypted[i]); 
+System.out.print((char)(encrypted[i]+96)); 
+} 
+for(i=0;i<nofelem;i++) 
+{ 
+decrypted[i]=1; 
+for(j=0;j<d;j++) 
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    int L[n1], R[n2];
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    int i = 0;
-    int j = 0;
-    int k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
-}
-
-void generateRandomArray(int arr[], int n, int min, int max) {
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % (max - min + 1) + min;
-    }
-}
-
-int main() {
-    FILE *fp;
-    fp = fopen("sorting_times.csv", "w");
-    if (fp == NULL) {
-        printf("Error opening file.\n");
-        return 1;
-    }
-
-    fprintf(fp, "n,Time taken (ms)\n");
-
-    srand(time(NULL));
-
-    int max_n = 10000;
-    int min = 1;
-    int max = 10000;
-
-    clock_t start, end;
-    double cpu_time_used;
-
-    for (int n = 5000; n <= max_n; n += 500) {
-        int arr[n];
-
-        generateRandomArray(arr, n, min, max);
-
-        start = clock();
-
-        mergeSort(arr, 0, n - 1);
-
-        end = clock();
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
-
-        printf("Time taken to sort %d elements: %.2f ms\n", n, cpu_time_used);
-
-        fprintf(fp, "%d,%.2f\n", n, cpu_time_used);
-    }
-
-    fclose(fp);
-
-    printf("Data saved to sorting_times.csv\n");
-
-    return 0;
-}
+decrypted[i]=(decrypted[i]*encrypted[i])%n; 
+} 
+System.out.println("\n Decrypted message\n "); 
+for(i=0;i<nofelem;i++) 
+System.out.print((char)(decrypted[i]+96)); 
+return; 
+} 
+} 
